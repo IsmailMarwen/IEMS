83b47e41b9a7e641984d80da215ea249a7a438c2
IsmailMarwen <ismail.marwen.ismail@gmail.com>
2023-11-29 00:18:32 +0100

    project spring

---
+++ TournoiFootball/src/main/java/com/jee/TournoiFootball/TournoiFootballApplication.java
@@ -0,0 +1,13 @@
+package com.jee.TournoiFootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournoiFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournoiFootballApplication.class, args);
+	}
+
+}
---
+++ TournoiFootball/src/test/java/com/jee/TournoiFootball/TournoiFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jee.TournoiFootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournoiFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
